7c19d684ba1e97b17a99d816fa6cb427e4caab3e
alpinj <alpinjestinera@yahoo.co.id>
2019-05-06 14:45:36 +0700

    Add lazada & shopee crawler

--- webcrawler/webcrawler/spiders/lazada.py
+++ webcrawler/webcrawler/spiders/lazada.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import scrapy
+import scrapy, datetime
 
 from webcrawler.items import ProductItem
 
@@ -30,8 +30,24 @@ class LazadaSpider(scrapy.Spider):
                     }
                 }
             })
+        
+        # next_page_object = response
 
     def parse_product(self, response):
-        yield{
-            'url' : response.url
-        }
+        product_object = ProductItem()
+        product_object['online_marketplace'] = self.name
+        product_object['time_taken'] = datetime.datetime.now()
+        product_object['url'] = response.url
+        product_object['title'] = response
+        product_object['title'] = response
+        product_object['image_url'] = response
+        product_object['price_final'] = response
+        product_object['rating'] = response
+        product_object['condition'] = response
+        product_object['seller'] = response
+        product_object['seller_url'] = response
+        product_object['seller_location'] = response
+        product_object['category'] = response
+        product_object['description'] = response
+        
+        yield product_object
--- webcrawler/webcrawler/spiders/shopee.py
+++ webcrawler/webcrawler/spiders/shopee.py
@@ -4,10 +4,38 @@ import scrapy
 
 class ShopeeSpider(scrapy.Spider):
     name = 'shopee'
-    allowed_domains = ['www.shopee.com']
-    start_urls = [
-        'https://shopee.co.id/Komputer-Aksesoris-cat.134?page=0&sortBy=ctime'
+
+    def start_requests(self):
+        urls = [
+            'https://shopee.co.id/Komputer-Aksesoris-cat.134?page=0&sortBy=ctime'
         ]
+        for url_item in urls:
+            yield scrapy.Request(url=url_item, callback=self.parse, meta={
+                'splash':{
+                    'args':{
+                        'html':1,
+                    }
+                }
+            })
 
     def parse(self, response):
         pass
+
+    def parse_product(self, response):
+        product_object = ProductItem()
+        product_object['online_marketplace'] = self.name
+        product_object['time_taken'] = datetime.datetime.now()
+        product_object['url'] = response.url
+        product_object['title'] = response
+        product_object['title'] = response
+        product_object['image_url'] = response
+        product_object['price_final'] = response
+        product_object['rating'] = response
+        product_object['condition'] = response
+        product_object['seller'] = response
+        product_object['seller_url'] = response
+        product_object['seller_location'] = response
+        product_object['category'] = response
+        product_object['description'] = response
+        
+        yield product_object
