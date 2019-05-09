b091ea3d1d0466b9aa9b684e7847f666c58e5642
alpinj <alpinjestinera@yahoo.co.id>
2019-05-10 00:57:36 +0700

    Update jd_id.py

--- webcrawler/webcrawler/spiders/jd_id.py
+++ webcrawler/webcrawler/spiders/jd_id.py
@@ -1,13 +1,38 @@
 # -*- coding: utf-8 -*-
-import scrapy
+import scrapy, datetime
 
+from webcrawler.items import ProductItem
 
 class JdIdSpider(scrapy.Spider):
     name = 'jd.id'
     allowed_domains = ['www.jd.id']
     start_urls = [
-        'http://www.jd.id/'
+        'https://www.jd.id/category/jual-pc-875061492.html'
         ]
 
     def parse(self, response):
-        pass
+        products = response.css('div.item div.p-desc')
+        for product_detail in products:
+            product_link = response.urljoin(product_detail.css('a::attr(href)').get())
+            yield scrapy.Request(url=product_link, callback=self.parse_product)
+        
+        next_page_object = response.urljoin(response.css('div.pagination a.p-next::attr(href)').get())
+        if(next_page_object is not None):
+            next_page = str(next_page_object)
+            yield scrapy.Request(url=next_page, callback=self.parse)
+
+    def parse_product(self, response):
+        product_object = ProductItem()
+        product_object['online_marketplace'] = self.name
+        product_object['time_taken'] = datetime.datetime.now()
+        product_object['url'] = response.url
+        product_object['title'] = response
+        product_object['image_url'] = response
+        product_object['price_final'] = response
+        product_object['rating'] = response
+        product_object['condition'] = response
+        product_object['seller'] = response
+        product_object['seller_url'] = response
+        product_object['seller_location'] = response
+        product_object['category'] = response
+        product_object['description'] = response
