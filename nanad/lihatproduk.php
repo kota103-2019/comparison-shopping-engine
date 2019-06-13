3b0ab42e3b89d5a53f1f433757fbdd8640bdb707
virly <nadhiravirly@gmail.com>
2019-06-13 22:22:42 +0700

    html page
    belom rapi hehe

---
+++ nanad/lihatproduk.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="css2.css">
+<style>
+
+</style>
+</head>
+<body>
+
+<div class="navbar">
+  <a href="#home">Home</a>
+  <a href="#news">Hot</a>
+  <div class="dropdown">
+    <button class="dropbtn">Kategori
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <!-- <div class="header">
+        <h2>Mega Menu</h2>
+      </div> -->
+      <div class="row">
+        <div class="column">
+          <h3>Handphone&Tablet</h3>
+          <a href="#">Handphone</a>
+          <a href="#">Tablet</a>
+          <a href="#">Sparepart&Tool</a>
+          <a href="#">Device Lain</a>
+          <a href="#">Lainnya</a>
+        </div>
+        <div class="column">
+          <h3>Komputer</h3>
+          <a href="#">Hardware</a>
+          <a href="#">Komponen</a>
+          <a href="#">Aksesoris</a>
+          <a href="#">Jaringan</a>
+          <a href="#">Storage</a>
+        </div>
+        <div class="column">
+          <h3>Elektronik</h3>
+          <a href="#">Kamera&Camcorder</a>
+          <a href="#">Televisi</a>
+          <a href="#">Audio</a>
+          <a href="#">Media Player</a>
+          <a href="#">Alat Pendingin</a>
+          <a href="#">Lainnya</a>
+        </div>
+        <div class="column">
+          <h3>Industrial</h3>
+          <a href="#">Mesin</a>
+          <a href="#">Safety</a>
+        </div>
+      </div>
+    <div class="row">
+      <div class="column">
+        <h3>Olahraga</h3>
+        <a href="#">Alat Olahraga</a>
+      </div>
+      <div class="column">
+        <h3>Rumah Tangga</h3>
+        <a href="#">Furniture</a>
+        <a href="#">Tempat Penyimpanan</a>
+        <a href="#">Alat Rumah Tangga</a>
+      </div>
+      <div class="column">
+        <h3>Perlengkapan Kantor</h3>
+        <a href="#">Alat Tulis Kantor</a>
+        <a href="#">Alat Kantor</a>
+      </div>
+      <div class="column">
+        <h3></h3>
+        <a href="#"></a>
+        <a href="#"></a>
+      </div>
+    </div>
+    </div>
+  </div>
+  <form action="#">
+    <input type="text" placeholder="Search" name="search" class="form-control">
+  </form>
+</div>
+
+<div style="padding:16px">
+  <p>Home > Handphone & Tablet > Handphone</p>
+  <h3>Samsung Galaxy A20 (ditemukan 108 item)</h3>
+</div>
+
+<div class="rowcontainer">
+  <div class="left">
+    <img src="hp.jpg" alt="Avatar" style="width:100%">
+    <h3>Deskripsi Produk</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  </div>
+  <div class="right">
+    <table>
+      <tr>
+        <th></th>
+        <th></th>
+      </tr>
+      <tr>
+        <td><h3>Nama Produk</h3></td>
+        <td><p>Nama Produk</p></td>
+      <tr>
+      <tr>
+        <td><h3>Harga Produk</h3></td>
+        <td><p>Harga Produk</p></td>
+      </tr>
+      <tr>
+        <td><h3>Kondisi Produk</h3></td>
+        <td><p>Kondisi Produk</p></td>
+      </tr>
+      <tr>
+        <td><h3>Last Update</h3></td>
+        <td><p>Last Update</p></td>
+      </tr>
+      <tr>
+        <td><h3>Diskon Produk</h3></td>
+        <td><p>Diskon Produk</p></td>
+      </tr>
+      <tr>
+        <td><h3>Terjual</h3></td>
+        <td><p>Terjual</p></td>
+      </tr>
+      <tr>
+        <td><h3>Rating</h3></td>
+        <td><p>Rating</p></td>
+      </tr>
+      <tr>
+        <td><h3>Jumlah Rating</h3></td>
+        <td><p>Jumlah Rating</p></td>
+      </tr>
+      <tr>
+        <td><h3>Penjual</h3></td>
+        <td><p>Penjual</p></td>
+      </tr>
+      <tr>
+        <td><h3>Lokasi Toko</h3></td>
+        <td><p>Lokasi Toko</p></td>
+      </tr>
+      <tr>
+        <td><h3>Online Marketplace</h3></td>
+        <td><p>Online Marketplace</p></td>
+      </tr>
+    </table>
+
+  </div>
+</div>
+</body>
+</html>
---
+++ nanad/test2.php
@@ -0,0 +1,258 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="css2.css">
+<!-- <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> -->
+<style>
+
+</style>
+</head>
+<body>
+
+<div class="navbar">
+  <a href="#home">Home</a>
+  <a href="#news">Hot</a>
+  <div class="dropdown">
+    <button class="dropbtn">Kategori
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <!-- <div class="header">
+        <h2>Mega Menu</h2>
+      </div> -->
+      <div class="row">
+        <div class="column">
+          <h3>Handphone&Tablet</h3>
+          <a href="#">Handphone</a>
+          <a href="#">Tablet</a>
+          <a href="#">Sparepart&Tool</a>
+          <a href="#">Device Lain</a>
+          <a href="#">Lainnya</a>
+        </div>
+        <div class="column">
+          <h3>Komputer</h3>
+          <a href="#">Hardware</a>
+          <a href="#">Komponen</a>
+          <a href="#">Aksesoris</a>
+          <a href="#">Jaringan</a>
+          <a href="#">Storage</a>
+        </div>
+        <div class="column">
+          <h3>Elektronik</h3>
+          <a href="#">Kamera&Camcorder</a>
+          <a href="#">Televisi</a>
+          <a href="#">Audio</a>
+          <a href="#">Media Player</a>
+          <a href="#">Alat Pendingin</a>
+          <a href="#">Lainnya</a>
+        </div>
+        <div class="column">
+          <h3>Industrial</h3>
+          <a href="#">Mesin</a>
+          <a href="#">Safety</a>
+        </div>
+      </div>
+    <div class="row">
+      <div class="column">
+        <h3>Olahraga</h3>
+        <a href="#">Alat Olahraga</a>
+      </div>
+      <div class="column">
+        <h3>Rumah Tangga</h3>
+        <a href="#">Furniture</a>
+        <a href="#">Tempat Penyimpanan</a>
+        <a href="#">Alat Rumah Tangga</a>
+      </div>
+      <div class="column">
+        <h3>Perlengkapan Kantor</h3>
+        <a href="#">Alat Tulis Kantor</a>
+        <a href="#">Alat Kantor</a>
+      </div>
+      <div class="column">
+        <h3></h3>
+        <a href="#"></a>
+        <a href="#"></a>
+      </div>
+    </div>
+    </div>
+  </div>
+  <form action="#">
+    <input type="text" placeholder="Search" name="search" class="form-control">
+  </form>
+</div>
+
+<div style="padding:16px">
+  <p>Home > Handphone & Tablet > Handphone</p>
+  <h3>Samsung Galaxy A20 (ditemukan 108 item)</h3>
+</div>
+<div class="rowcontainer">
+  <div class="left">
+    <h3>Filter</h3>
+    <form action="#">
+      <label for="fname">Harga Minimum</label><br>
+      <input type="number" name="minprice" placeholder="Minimum"><br>
+      <label for="fname">Harga Maksimum</label><br>
+      <input type="number" name="maxprice" placeholder="Minimum"><br>
+      <input type="submit" value="Terapkan">
+    </form>
+    <form class="" action="#">
+      <input type="submit" name="reset" value="Reset">
+    </form>
+    <form class="" action="#">
+      <input type="submit" name="compare" value="Bandingkan">
+    </form>
+  </div>
+
+  <div class="right">
+    <div class="rowcontain">
+      <div class="card-info">
+        <h2>Samsung Galaxy A20</h2>
+        <table>
+          <tr>
+            <th>Harga Terendah</th>
+            <th>Harga Tertinggi</th>
+          </tr>
+          <tr>
+            <td>Rp 2.000.000</td>
+            <td>Rp 2.900.000</td>
+          </tr>
+          <tr>
+            <th>Harga Rata-Rata</th>
+            <th>Harga Tengah</th>
+          </tr>
+          <tr>
+            <td>Rp 00000</td>
+            <td>Rp 00000</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+    <div class="rowcontain">
+    <div class="cardcontainer">
+      <div class="card-produk">
+        <img src="hp.jpg" alt="Avatar" style="width:100%">
+        <div class="cardcontentcontainer">
+          <h4><b>Samsung Galaxy A20</b></h4>
+          <p>Baru | Bandung</p>
+          <p>Rp 2.500.000</p>
+          <p>Rp Shopee</p>
+          <form action="#"><button class="button button2">Kunjungi Toko</button></form><br>
+          <form action="lihatproduk.php"><button type="submit" class="button button3">Lihat Detail</button></form><br>
+          <label class="">Bandingkan
+            <input type="checkbox" checked="checked">
+            <span class="checkmark"></span>
+          </label>
+        </div>
+      </div>
+      <div class="card-produk">
+        <img src="hp.jpg" alt="Avatar" style="width:100%">
+        <div class="cardcontentcontainer">
+          <h4><b>Samsung Galaxy A20</b></h4>
+          <p>Baru | Bandung</p>
+          <p>Rp 2.500.000</p>
+          <p>Rp Shopee</p>
+          <form action="#"><button class="button button2">Kunjungi Toko</button></form><br>
+          <form action="lihatproduk.php"><button type="submit" class="button button3">Lihat Detail</button></form><br>
+          <label class="">Bandingkan
+            <input type="checkbox" checked="checked">
+            <span class="checkmark"></span>
+          </label>
+        </div>
+      </div>
+      <div class="card-produk">
+        <img src="hp.jpg" alt="Avatar" style="width:100%">
+        <div class="cardcontentcontainer">
+          <h4><b>Samsung Galaxy A20</b></h4>
+          <p>Baru | Bandung</p>
+          <p>Rp 2.500.000</p>
+          <p>Rp Shopee</p>
+          <form action="#"><button class="button button2">Kunjungi Toko</button></form><br>
+          <form action="lihatproduk.php"><button type="submit" class="button button3">Lihat Detail</button></form><br>
+          <label class="">Bandingkan
+            <input type="checkbox" checked="checked">
+            <span class="checkmark"></span>
+          </label>
+        </div>
+      </div>
+      <div class="card-produk">
+        <img src="hp.jpg" alt="Avatar" style="width:100%">
+        <div class="cardcontentcontainer">
+          <h4><b>Samsung Galaxy A20</b></h4>
+          <p>Baru | Bandung</p>
+          <p>Rp 2.500.000</p>
+          <p>Rp Shopee</p>
+          <form action="#"><button class="button button2">Kunjungi Toko</button></form><br>
+          <form action="lihatproduk.php"><button type="submit" class="button button3">Lihat Detail</button></form><br>
+          <label class="">Bandingkan
+            <input type="checkbox" checked="checked">
+            <span class="checkmark"></span>
+          </label>
+        </div>
+      </div>
+      <div class="card-produk">
+        <img src="hp.jpg" alt="Avatar" style="width:100%">
+        <div class="cardcontentcontainer">
+          <h4><b>Samsung Galaxy A20</b></h4>
+          <p>Baru | Bandung</p>
+          <p>Rp 2.500.000</p>
+          <p>Rp Shopee</p>
+          <form action="#"><button class="button button2">Kunjungi Toko</button></form><br>
+          <form action="lihatproduk.php"><button type="submit" class="button button3">Lihat Detail</button></form><br>
+          <label class="">Bandingkan
+            <input type="checkbox" checked="checked">
+            <span class="checkmark"></span>
+          </label>
+        </div>
+      </div>
+      <div class="card-produk">
+        <img src="hp.jpg" alt="Avatar" style="width:100%">
+        <div class="cardcontentcontainer">
+          <h4><b>Samsung Galaxy A20</b></h4>
+          <p>Baru | Bandung</p>
+          <p>Rp 2.500.000</p>
+          <p>Rp Shopee</p>
+          <form action="#"><button class="button button2">Kunjungi Toko</button></form><br>
+          <form action="lihatproduk.php"><button type="submit" class="button button3">Lihat Detail</button></form><br>
+          <label class="">Bandingkan
+            <input type="checkbox" checked="checked">
+            <span class="checkmark"></span>
+          </label>
+        </div>
+      </div>
+      <div class="card-produk">
+        <img src="hp.jpg" alt="Avatar" style="width:100%">
+        <div class="cardcontentcontainer">
+          <h4><b>Samsung Galaxy A20</b></h4>
+          <p>Baru | Bandung</p>
+          <p>Rp 2.500.000</p>
+          <p>Rp Shopee</p>
+          <form action="#"><button class="button button2">Kunjungi Toko</button></form><br>
+          <form action="lihatproduk.php"><button type="submit" class="button button3">Lihat Detail</button></form><br>
+          <label class="">Bandingkan
+            <input type="checkbox" checked="checked">
+            <span class="checkmark"></span>
+          </label>
+        </div>
+      </div>
+      <div class="card-produk">
+        <img src="hp.jpg" alt="Avatar" style="width:100%">
+        <div class="cardcontentcontainer">
+          <h4><b>Samsung Galaxy A20</b></h4>
+          <p>Baru | Bandung</p>
+          <p>Rp 2.500.000</p>
+          <p>Rp Shopee</p>
+          <form action="#"><button class="button button2">Kunjungi Toko</button></form><br>
+          <form action="lihatproduk.php"><button type="submit" class="button button3">Lihat Detail</button></form><br>
+          <label class="">Bandingkan
+            <input type="checkbox" checked="checked">
+            <span class="checkmark"></span>
+          </label>
+        </div>
+      </div>
+    </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
